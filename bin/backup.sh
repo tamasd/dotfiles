e64d235079cd3d2c74d07527726ff3f8bc04aaa9
Tamás Demeter-Haludka <tamas@demeter-haludka.hu>
2024-05-15 22:12:25 +0200

    exclude zig build dirs from backup

--- bin/backup.sh
+++ bin/backup.sh
@@ -6,7 +6,7 @@ cd $HOME
 
 FILENAME=backup-$(hostname).tar.gz.gpg
 
-tar --exclude=target --exclude=vendor --exclude=node_modules -cf - Projects | \
+tar --exclude=target --exclude=vendor --exclude=node_modules --exclude=zig-cache --exclude=zig-out -cf - Projects | \
 	gzip -9 | \
 	gpg --encrypt -r AECD4A22A8C7F19583C0B03ECCAD8960035C19E0 > /tmp/$FILENAME
 
