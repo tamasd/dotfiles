dc1395e4d07845d18cfaf5ce5e8969cf6fa1cc30
Tamás Demeter-Haludka <tamas@demeter-haludka.hu>
2026-01-26 20:09:11 +0100

    config update

--- battery.sh
+++ battery.sh
@@ -4,6 +4,8 @@ if ! command -v upower &>/dev/null ; then
 	exit 0
 fi
 
+export LC_ALL=C
+
 info="$(upower -e | grep 'DisplayDevice' | xargs upower -i)"
 percentage="$(echo "$info" | grep percentage | cut -d':' -f2 | tr -d ' ')"
 rate=$(echo "$info" | grep energy-rate | cut -d':' -f2 | tr -d ' ' | cut -d'W' -f1 | cut -d'.' -f1)
--- temp.sh
+++ temp.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 
-val=$(cat /sys/class/thermal/thermal_zone*/temp | sort -nr | head -n 1)
+val=$(cat /sys/class/thermal/thermal_zone*/temp | awk '$1 < 105000' | sort -nr | head -n 1)
 
 echo -e "$((val / 1000))°C"
